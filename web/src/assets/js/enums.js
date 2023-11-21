50864945fafd312d90996009193800a53757a419
jinchenxing <2541226493@qq.com>
2023-11-21 16:52:36 +0800

    生成枚举类

--- web/src/assets/js/enums.js
+++ web/src/assets/js/enums.js
@@ -1,3 +1,9 @@
+PASSENGER_TYPE = {
+  AUDIT: { key: '1', label: '成人' },
+  CHILD: { key: '2', label: '儿童' },
+  STUDENT: { key: '3', label: '学生' }
+}
+
 PASSENGER_TYPE_ARRAY = [
   { key: '1', label: '成人' },
   { key: '2', label: '儿童' },
