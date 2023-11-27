82d6927759e9b195327ab890f2575b07244201c5
jinchenxing <2541226493@qq.com>
2023-11-27 14:41:59 +0800

    订单信息生成

---
+++ business/src/main/java/top/flobby/train/business/controller/admin/ConfirmOrderAdminController.java
@@ -0,0 +1,39 @@
+package top.flobby.train.business.controller.admin;
+
+import top.flobby.train.common.context.LoginMemberContext;
+import top.flobby.train.common.resp.CommonResp;
+import top.flobby.train.common.resp.PageResp;
+import top.flobby.train.business.req.ConfirmOrderQueryReq;
+import top.flobby.train.business.req.ConfirmOrderSaveReq;
+import top.flobby.train.business.resp.ConfirmOrderQueryResp;
+import top.flobby.train.business.service.ConfirmOrderService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/confirm-order")
+public class ConfirmOrderAdminController {
+
+    @Resource
+    private ConfirmOrderService confirmOrderService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody ConfirmOrderSaveReq req) {
+        confirmOrderService.save(req);
+        return CommonResp.success();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<ConfirmOrderQueryResp>> queryList(@Valid ConfirmOrderQueryReq req) {
+        PageResp<ConfirmOrderQueryResp> list = confirmOrderService.queryList(req);
+        return CommonResp.success(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        confirmOrderService.delete(id);
+        return CommonResp.success();
+    }
+
+}
